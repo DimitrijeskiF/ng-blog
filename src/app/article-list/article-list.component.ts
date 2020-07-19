77c237786df4cf26f4b981ea00e02d6ca317555d
Filip Dimitrijeski <filipdimitrijeski7@gmail.com>
2020-07-19 17:46:39 +0200

    Adding Article to Blog

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,3 +1,5 @@
+import { ARTICLES } from './../mock-articles';
+import { Article } from './../article';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleListComponent implements OnInit {
 
+ article: Article[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.article = ARTICLES;
+    console.log(this.article);
   }
 
 }
---
+++ src/app/mock-articles.ts
@@ -0,0 +1,21 @@
+import { Article } from './article';
+export const ARTICLES: Article []  = [
+    {
+        id: 1,
+        title: 'My First Article',
+        content: '',
+        decription: 'This is my first article, It is great to read thet',
+        key: 'my-first-article',
+        date: new Date(),
+        imageUrl: 'https://angular.io/assets/images/logos/angular/angular.png'
+    },
+    {
+        id: 2,
+        title: 'My Second Article',
+        content: '',
+        decription: 'This is my Second article, It is great to read thet',
+        key: 'my-second-article',
+        date: new Date(),
+        imageUrl: 'https://angular.io/assets/images/logos/angular/angular.png'
+    }
+];
