23c11cc9bcb9a2c86ac81a3bfcd590f3b2eb1638
Filip Dimitrijeski <filipdimitrijeski7@gmail.com>
2020-07-19 21:49:01 +0200

    ArticleComponent And Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
+import { ArticleComponent } from './article/article.component';
 import { AboutComponent } from './about/about.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', component: ArticleListComponent},
   {path: 'article', component: ArticleListComponent},
   {path: 'about', component: AboutComponent},
-  
+  {path: '', component: ArticleListComponent},
+  {path: ':key', component: ArticleComponent},
 ];
 
 @NgModule({
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../article';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  article: Article = null;
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params
+      .subscribe(params => {
+        this.article = new Article();
+        this.article.key = params.key;
+      });
+  }
+}
